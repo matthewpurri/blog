41acac74517d8dde25dd6261dca5ba7e8b3dc753
Matthew Purri <matthew.purri@rutgers.edu>
2020-05-31 17:31:39 -0400

    Update Travis Env Var

---
+++ content/posts/my-first-post.md
@@ -0,0 +1,9 @@
+---
+title: "My First Post"
+date: 2020-05-31T16:19:12-04:00
+draft: false
+---
+
+Hello world,
+
+This is a test post.
--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 cd public
 
-if [ -n "$GH_AUTH_SEC" ]
+if [ -n "$GITHUB_AUTH_SECRET" ]
 then
     touch ~/.git-credentials
     chmod 0600 ~/.git-credentials
-    echo $GH_AUTH_SEC > ~/.git-credentials
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials
 
     git config credential.helper store
     git config user.email "matthewpurri-blog-bot@users.noreply.github.com"
